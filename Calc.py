cd68f4aa570f96f4ef52e720c791fad5bdb90a6c
Den0l <136687221+Den0l@users.noreply.github.com>
2023-09-29 09:00:20 +0300

    Add files via upload

--- Calc.py
+++ Calc.py
@@ -6,7 +6,7 @@ while True:
        n1 = float(n1)
     else:
         n1 = str(n1)
-    if (str(n1).isnumeric()) or (n1 in ["cos", "sin", "tg", "sqrt"]) or (isinstance(n1, float)):
+    if (str(n1).isnumeric()) or (n1 in ["cos", "sin", "tg", "sqrt"]) or (isinstance(n1, float) or ('-')):
         if n1 in ["cos", "sin", "tg", "sqrt"]:
             z = input("Введите число: ")
             if z == '': break  
@@ -26,7 +26,7 @@ while True:
             elif n1 == 'cos': print(math.cos(float(z))) #косинус
             elif n1 == 'sin': print(math.sin(float(z))) #синус
             elif n1 == 'tg': print(math.tan(float(z))) #тангенс
-        elif (str(n1).isnumeric()) or (isinstance(n1, float)):    
+        elif (str(n1).isnumeric()) or (isinstance(n1, float) or ('-')):    
             z = input("Введите знак(+, -, *, /, ^, !): ")
             if z == '': break
             while True:  
@@ -34,9 +34,10 @@ while True:
                     print("Неверные данные, повторите ввод: ")
                     z = input()
                 else: break
-            if z == '!': 
-                print(math.factorial(int(n1)))#факториал 
-                continue
+            if z == '!':  
+                    print(math.factorial(int(n1)))#факториал 
+                    continue
+
             n2 = input("Введите 2 число: ") 
             if "." in n2: 
                 n2 = float(n2)
@@ -44,7 +45,7 @@ while True:
                 n2 = str(n2)
             if n2 == '': break
             while True:
-                if (str(n2).isnumeric()) or (isinstance(n2, float)):
+                if (str(n2).isnumeric()) or (isinstance(n2, float) or ('-')):
                     break
                 else:
                     print("Неверные данные, повторите ввод: ")
